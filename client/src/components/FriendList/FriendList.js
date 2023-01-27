e3a9e043876e7f2bd162f7706ab4fb4f4b466a36
rsheridan613 <rsheridan613@gmail.com>
2023-01-27 14:48:23 -0700

    added file for FriendList. starter code for what it'll need

---
+++ client/src/components/FriendList/FriendList.js
@@ -0,0 +1,10 @@
+import { Typography, Modal, Box, Button, IconButton } from "@mui/material";
+
+import { QUERY_ME } from "../../utils/queries";
+import { REMOVE_FRIEND } from "../../utils/mutations";
+
+const FriendList = () => {
+  return;
+};
+
+export default FriendList;
